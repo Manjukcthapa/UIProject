b32eb9a282ae5c08f61b2fe16666d782017c5fb7
Manju KC <Manju KC>
2019-06-13 18:46:34 -0400

    Updated the app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { NavLink, Route } from "react-router-dom";
-import LogIn from "../src/Auth/LogIn.js";
+import LogIn from "./auth/LogIn.js";
 import UserList from './Users/User.js'
 import "./App.css";
 
@@ -12,6 +12,7 @@ class App extends Component {
         <nav>
         <NavLink to="/login">LogIn</NavLink>
         <NavLink to="/user">UserList</NavLink>
+        <button onClick={this.logout}>Logout</button>
         </nav>
         <div>
         <Route path="/login" component={LogIn} />
